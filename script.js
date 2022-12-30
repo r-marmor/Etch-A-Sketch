a56b19ad4decc5bf94fc8f3bd5f3b2e5ea532728
r-marmor <raphael.marmor@gmail.com>
2022-12-30 09:50:18 +0100

    small changes on for loop

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-let color = 'black'
-let click = true;
+let color = 'black';
+const board = document.querySelector(".board");
 
 let populateBoard = (size) => {
-    let board = document.querySelector(".board");
-    let squares = board.querySelectorAll('div');
-    squares.forEach(div => div.remove());
+    // let squares = board.querySelectorAll('div');
+    // squares.forEach(div => div.remove());
     board.style.gridTemplateColumns = `repeat(${size},  1fr)`;
     board.style.gridTemplateRows = `repeat(${size},  1fr)`;
 
-    let amount = size * size;
-    for(let i = 0; i < amount; i++) {
+    let i = 0;
+    while (i < size * size) {
         let square = document.createElement('div');
         square.addEventListener('mouseover', colorSquare);
         square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
+        i++;
     }
-}
+};
 
-function colorSquare() {
-    if (click) {
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function colorSquare(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
         if (color === 'random') {
-            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+            const randomR = Math.floor(Math.random() * 256);
+            const randomG = Math.floor(Math.random() * 256);
+            const randomB = Math.floor(Math.random() * 256);
+            this.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
         } else {
             this.style.backgroundColor = color;
         }
     }
-}
 
 function changeSize(input) {
     if (input >= 2 && input <= 100) {
@@ -35,27 +41,15 @@ function changeSize(input) {
     else {
         document.querySelector('.error').style.display = 'flex';
     }
-};
+}
 
 function changeColor(choice) {
     color = choice;
 }
 
 function resetBoard() {
-    let board = document.querySelector(".board");
     let squares = board.querySelectorAll('div');
     squares.forEach(div => div.style.backgroundColor = 'white');
 }
-
-document.querySelector('body').addEventListener('click', (e) => {
-    if (e.target.tagName === "DIV") {
-        click = !click;
-        if (click) {
-            document.querySelector('.coloring').textContent = "Mode: Coloring";
-        } else {
-            document.querySelector('.coloring').textContent = "Mode: Not Coloring";
-        }
-    }   
-});
 
 populateBoard(16);
